dc658206aaff4c1cd0e8654fdf3f9a1aaafffe9b
keithmacdonalddev <tenantbureau6@gmail.com>
2021-05-14 08:58:50 -0400

    add link to project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
 	return (
 		<div>
-			<a href='https://www.reacttracker.com' target='_blank'>
+			<a href='https://www.reacttracker.com' target='_blank' rel='noreferrer'>
 				React Tracker
 			</a>
 		</div>
